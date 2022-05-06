89c4722024e67dcd63c75060101046cb227092e2
JHONATAN BILBAO USCCA GIRALDO <104391558+JhonatanUscca@users.noreply.github.com>
2022-05-05 21:57:03 -0500

    Update maxProduct.cpp

--- EjerciciosClase/Semana03/maxProduct.cpp
+++ EjerciciosClase/Semana03/maxProduct.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
-using namespace std;
+#include <algorithm>
+#include <vector>
 int main(){
-    int n,max1=0,max2=0;
-    cout<<"Numero? ";
+    vector<int> vec;
+    int n,num;
+    cout<<"Numero de elemetos? ";
     cin>>n;
-    int A[n];
     for(int i=0;i<n;i++){
-        cout<<"Numero "<<i+1<<"? ";
-        cin>>A[i];
-        if(A[i]>max1){
-            max1=A[i];
-        }
+        cout<<"Numero "<<i+1<<" : ";
+        cin>>num;
+        vec.push_back(num);
     }
-    for(int i=0;i<n;i++){
-        cout<<A[i]<<" ";
-        if(A[i]!=max1){
-            if(A[i]>max2){
-                max2=A[i];
-            }
-        }
-    }
+
     cout<<endl;
-    cout<<"Resultado: "<<max1*max2; 
-}
+    sort(vec.begin(), vec.end());
+    cout<<"Resultao: "<<vec[n-1]*vec[n-2];
+
+} 
