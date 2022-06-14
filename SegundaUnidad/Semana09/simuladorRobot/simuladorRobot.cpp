0e52d679ae754ef3d79013193862470858c9e984
JHONATAN BILBAO USCCA GIRALDO <104391558+JhonatanUscca@users.noreply.github.com>
2022-06-13 20:14:49 -0500

    Create simuladorRobot.cpp

---
+++ SegundaUnidad/Semana09/simuladorRobot/simuladorRobot.cpp
@@ -0,0 +1,12 @@
+#include "simuladorRobot.h"
+
+int main(){
+    simuladorRobot robot1;
+    robot1.incertarDatos();
+    robot1.incertarInstrucciones();
+    robot1.covertir();
+    robot1.leer();
+    robot1.resultados();
+    return 0;
+
+}
