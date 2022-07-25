8269e7af104ead7fb0534ea460402f49a973c87e
JHONATAN BILBAO USCCA GIRALDO <104391558+JhonatanUscca@users.noreply.github.com>
2022-07-25 09:58:57 -0500

    Create LinkedList.h

---
+++ TerceraUnidad/Semana15/e3asignacion/LinkedList.h
@@ -0,0 +1,32 @@
+#ifndef __LINKEDLIST_H__
+#define __LINKEDLIST_H__
+
+#include <iostream>
+#include "Node.h"
+
+class LinkedList {
+        int size;
+        Node *head;
+    public:
+
+        LinkedList();
+        LinkedList(int);
+        LinkedList(const LinkedList &);
+
+        ~LinkedList();
+
+        void insert(int);
+
+        void removeByValue(int);
+        void removeByPosition(int);
+        
+        bool search(int value);
+        
+        friend std::ostream& operator <<(std::ostream &exit,const LinkedList& C);
+        friend LinkedList& operator +(const LinkedList& A, const LinkedList &B);
+        LinkedList& operator +=(const LinkedList& A);
+};
+
+#include "LinkedList.cpp"
+
+#endif
